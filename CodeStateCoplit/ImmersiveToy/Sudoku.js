54545ec1bf0d6d1195754739366fb277434a4099
KEUMYUNSU <rmadbstn@naver.com>
2023-04-13 10:10:18 +0900

    uncomplete

--- CodeStateCoplit/ImmersiveToy/Sudoku.js
+++ CodeStateCoplit/ImmersiveToy/Sudoku.js
@@ -1,26 +1,9 @@
 const sudoku = function (board) {
     // TODO: 여기에 코드를 작성합니다.
 
-    const startSudoku = function (board) {
+    
 
-        for (let i = 0; i < board.length; i++) {
-
-            for (let j = 0; j < board[i].length; j++) {
-
-
-                if(board[i][j]===0){
-
-
-                    checkBoard(i,j)
-                    
-
-                }
-
-
-
-            }
-        }
-    }
+    //1. 
 
 
     
