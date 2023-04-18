4c52aa0ebdd5e4a368fd4431eea4153be0fc38c7
KEUMYUNSU <rmadbstn@naver.com>
2023-04-18 09:50:55 +0900

    2022-04-18

--- CodeStateCoplit/ImmersiveToy/Power.js
+++ CodeStateCoplit/ImmersiveToy/Power.js
@@ -2,11 +2,11 @@ function power(base, exponent) {
     if (exponent === 0) return 1;
 
     const half = parseInt(exponent / 2);
-    const temp = power(base, ihalf);
+    const temp = power(base, half);
     const result = (temp * temp) % 94906249;
 
     if (exponent % 2 === 1) return (base * result) % 94906249;
     else return result;
 }
 let output = power(3, 40);
-console.log(output);
+console.log(output); 
