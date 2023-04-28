ad09f846636da782d20603deb7b9fd2cd7a1ce33
KEUMYUNSU <rmadbstn@naver.com>
2023-04-28 09:37:59 +0900

    PowerSet COMPLETE

--- CodeStateCoplit/ImmersiveToy/PowerSet.js
+++ CodeStateCoplit/ImmersiveToy/PowerSet.js
@@ -5,14 +5,14 @@ const powerSet = function (str) {
 
     //중복 제거
 
-    let result = [''];
+    let result = [];
 
     for(let i = 0; i<arr.length-1;i++){
 
         if(arr[i]===arr[i+1]){
 
             arr.splice(i,1);
-
+            i--;
         }
 
     }
@@ -22,22 +22,23 @@ const powerSet = function (str) {
 
     let addStr = '';
 
-    function maker(){
+    function maker(startIndex){
  
         result.push(addStr);
 
         if(arr.length<=addStr.length){
+            
             return;
         } 
 
-        for(let i=0;i<checkArr.length;i++){
+        for(let i=startIndex;i<checkArr.length;i++){
 
             if(!checkArr[i]){
                 console.log('check');
                 addStr+=arr[i];
                 checkArr[i] = true;
-                maker();
-                addStr.slice(addStr.length-2,1);
+                maker(i+1);
+                addStr = addStr.substring(0,addStr.length-1);
                 checkArr[i] = false;
 
             }
@@ -47,11 +48,11 @@ const powerSet = function (str) {
         }
 
     }
-    maker(); 
+    maker(0); 
     console.log(arr);
     return result;
 };
 
 
-output1 = powerSet('jjumpu');
+output1 = powerSet('bbaaa');
 console.log(output1); 
