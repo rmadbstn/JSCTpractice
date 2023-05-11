2a3fac97c4b118a1979c5bcbaba347fa70048710
KEUMYUNSU <rmadbstn@naver.com>
2023-05-11 10:10:03 +0900

    QuickSort ING

---
+++ CodeStateCoplit/ImmersiveToy/QuickSort.js
@@ -0,0 +1,17 @@
+const quickSort = function (arr) {
+    // TODO: 여기에 코드를 작성합니다.
+
+    let pivot= arr[0]; // pivot값은 일단 배열 첫번 째 값으로 함.
+
+
+
+  };
+
+
+
+
+const part = function(arr){ //나눠야하지않니?.
+
+
+}
+  
