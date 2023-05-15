d1f186402e0fa36638a62d9b0672a464738106ef
KEUMYUNSU <rmadbstn@naver.com>
2023-05-15 09:41:50 +0900

    QuickSort Complete

--- CodeStateCoplit/ImmersiveToy/QuickSort.js
+++ CodeStateCoplit/ImmersiveToy/QuickSort.js
@@ -1,17 +1,34 @@
-const quickSort = function (arr) {
-    // TODO: 여기에 코드를 작성합니다.
+// naive solution
+// const quickSort = function (arr) {
+//   if (arr.length <= 1) return arr;
 
-    let pivot= arr[0]; // pivot값은 일단 배열 첫번 째 값으로 함.
+//   const pivot = arr[0];
+//   const left = [];
+//   const right = [];
 
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] <= pivot) left.push(arr[i]);
+//     else right.push(arr[i]);
+//   }
 
+//   const lSorted = quickSort(left);
+//   const rSorted = quickSort(right);
+//   return [...lSorted, pivot, ...rSorted];
+// };
 
-  };
-
-
-
-
-const part = function(arr){ //나눠야하지않니?.
-
-
-}
+function quickSort(arr, transform = (item) => item) {
+    if (arr.length <= 1) return arr;
   
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+  
+    for (let i = 1; i < arr.length; i++) {
+      if (transform(arr[i]) < transform(pivot)) left.push(arr[i]);
+      else right.push(arr[i]);
+    }
+  
+    const lSorted = quickSort(left, transform);
+    const rSorted = quickSort(right, transform);
+    return [...lSorted, pivot, ...rSorted];
+  }
