aeb8a4f31e319383d16622f9f283709576310d40
KEUMYUNSU <rmadbstn@naver.com>
2023-06-01 09:58:07 +0900

    2022/06/01

--- CodeStateCoplit/ImmersiveToy/RotateMaxrix.js
+++ CodeStateCoplit/ImmersiveToy/RotateMaxrix.js
@@ -1,22 +1,28 @@
-const rotateMatrix = function (matrix) {
+const rotateMatrix = function (matrix, k = 1) { // k : 남은 회전 수
+
+
+    let arr = [];
+
+    if (matrix.length === 0) {
+        return [];
+    }
 
     let N = matrix.length;
-    let arr = [];
-    
-    for(let i = 0 ; i<N;i++){
+    let M = matrix[0].length;
+    for (let i = 0; i < M; i++) {//가로.
 
 
         let innerArr = [];
 
-        for(let j = 1 ; j <= N;j++){
+        for (let j = 1; j <= N; j++) { // 세로.
 
 
-            
+
             // matrix[matrix.length-j][i];
-            innerArr.push(matrix[N-j][i]);
+            innerArr.push(matrix[N - j][i]);
 
 
-        }    
+        }
 
         arr.push(innerArr);
 
@@ -24,8 +30,13 @@ const rotateMatrix = function (matrix) {
 
 
     }
+    k--;
 
-    return arr;
+    if (k === 0 || k === undefined) {
+        return arr;
+    } else {
+        rotateMatrix(arr, k);
+    }
 };
 
 const matrix = [
@@ -33,10 +44,10 @@ const matrix = [
     [5, 6, 7, 8],
     [9, 10, 11, 12],
     [13, 14, 15, 16],
-  ];
-  console.log(matrix[0][0]); // --> 1
-  console.log(matrix[3][2]); // --> 15
-  
-  const rotatedMatrix = rotateMatrix(matrix);
-  console.log(rotatedMatrix[0][0]); // --> 13
-  console.log(rotatedMatrix[3][2]); // --> 8
+];
+console.log(matrix[0][0]); // --> 1
+console.log(matrix[3][2]); // --> 15
+
+const rotatedMatrix = rotateMatrix(matrix,1);
+console.log(rotatedMatrix[0][0]); // --> 13
+console.log(rotatedMatrix[3][2]); // --> 8
